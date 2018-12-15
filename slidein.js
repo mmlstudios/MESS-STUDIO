061d7b76ef60c5dfddf9df3709e8f2ea127d14fc
Mads Møller Laursen <eaamml@students.eaaa.dk>
2018-12-15 13:52:57 +0100

    Upload af virkende filer

--- slidein.js
+++ slidein.js
@@ -1,13 +1,58 @@
 $(document).ready(function(){
     $("#leftToRight").click(function(){
-        $("#products").animate({left: '100%'}).css("display", "block");
+        $("#product-page").animate({left: '100%'}).css("display", "block");
     });
 });
 
 
 $(document).ready(function(){
-    $("#backbtn").click(function(){
-        $("#products").animate({left: '-100%'}).fadeOut(100);
+    $("#backbtn-product").click(function(){
+        $("#product-page").animate({left: '-100%'}).fadeOut(100);
     });
 });
 
+
+$(document).ready(function(){
+    $("#bottomToTop").click(function(){
+        $("#about-page").animate({top: '-100%'}).css("display", "block");
+    });
+});
+
+
+$(document).ready(function(){
+    $("#backbtn-about").click(function(){
+        $("#about-page").animate({top: '100%'}).fadeOut(250);
+    });
+});
+
+
+$(document).ready(function(){
+    $("#rightToLeft").click(function(){
+        $("#contact-page").animate({left: '-100%'}).css("display", "block");
+    });
+});
+
+
+$(document).ready(function(){
+    $("#backbtn-contact").click(function(){
+        $("#contact-page").animate({left: '100%'}).fadeOut(100);
+    });
+});
+
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
---
+++ slides.js
@@ -0,0 +1,16 @@
+$("#about-btn").click(function() {
+  $('.transform').toggleClass('transform-active');
+});
+
+$("#about-btn").click(function() {
+  $('.gone').toggleClass('gone-active');
+});
+
+$("#about-bck").click(function() {
+  $('.transform').toggleClass('transform-active');
+});
+
+
+$("#about-bck").click(function() {
+  $('.gone').toggleClass('gone-active');
+});
