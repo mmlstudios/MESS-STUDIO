1f0590572ae2e6f75f546bf5c4618e0fc7a9bc6d
Mads Møller Laursen <eaamml@students.eaaa.dk>
2018-12-16 21:10:34 +0100

    comments added

--- animation.js
+++ animation.js
@@ -1,22 +1,27 @@
+//------------------------product in and out---------------------
 $(document).ready(function(){
     $("#leftToRight").click(function(){
         $("#product-page").animate({left: '100%'}).css("display", "block");
     });
 });
-
+//Flytter produkt siden ind til den har en left property på 100% og ændrer display fra none til block
 
 $(document).ready(function(){
     $("#backbtn-product").click(function(){
         $("#product-page").animate({left: '-100%'}).fadeOut(100);
     });
 });
+//Flytter produkt siden ind til den har en left property på -100%, ændrer display fra block til none med en fadeout på 100 ms
 
 
+//------------------------about up and down---------------------
 $(document).ready(function(){
     $("#bottomToTop").click(function(){
         $("#about-page").animate({top: '-100%'}).css("display", "block");
     });
 });
+//Flytter about siden ind til den har en top property på -100% og ændrer display fra none til block
+
 
 
 $(document).ready(function(){
@@ -24,36 +29,49 @@ $(document).ready(function(){
         $("#about-page").animate({top: '100%'}).fadeOut(250);
     });
 });
+//Flytter produkt siden ind til den har en top property på 100%, ændrer display fra block til none med en fadeout på 250 ms
 
 
+//------------------------contact in and out---------------------
 $(document).ready(function(){
     $("#rightToLeft").click(function(){
         $("#contact-page").animate({left: '-100%'}).css("display", "block");
     });
 });
-
+//Flytter produkt siden ind til den har en left property på -100% og ændrer display fra none til block
 
 $(document).ready(function(){
     $("#backbtn-contact").click(function(){
         $("#contact-page").animate({left: '100%'}).fadeOut(100);
     });
 });
+//Flytter produkt siden ind til den har en left property på 100%, ændrer display fra block til none med en fadeout på 100 ms
 
 
-$("#teaser-vid").prop('muted', true);
+//----------------------Mute unmute button-----------------------
+
+//taget fra: stackoverflow.com/questions/26478062/html-video-mute-button
+
+$("#teaser-vid").prop('muted', true); //muted som udgangspunkt
 
 $(".mute-video").click(function () {
     if ($("#teaser-vid").prop('muted')) {
         $("#teaser-vid").prop('muted', false);
-        $(this).addClass('unmute-video');
+        $(this).addClass('unmute-video'); 
+        //skifter logo
 
     } else {
         $("#teaser-vid").prop('muted', true);
-        $(this).removeClass('unmute-video');
+        $(this).removeClass('unmute-video'); //skifter logo tilbage
     }
     console.log($("#teaser-vid").prop('muted'))
 });
 
+
+//------------------------Back to top button---------------------
+
+
+//taget fra: www.w3schools.com/howto/howto_js_scroll_to_top.asp
 
 
 // When the user scrolls down 20px from the top of the document, show the button
